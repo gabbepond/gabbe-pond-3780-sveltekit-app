deda644aa6938f33d38cf813f6c36cdf0b7fa13c
Gabbe Pond <114202309+gabbepond@users.noreply.github.com>
2025-01-26 23:21:29 -0700

    changes

---
+++ src/movies/+page.server.ts
@@ -0,0 +1,51 @@
+import clientPromise from '$lib/mongodb/mongodb.client';
+import type { PageServerLoad } from './$types';
+import type { Movie } from '$lib/types/Movie';
+
+export const load: PageServerLoad = async () => {
+	let movies: Movie[] | undefined;
+	let client;
+
+	try {
+		client = await clientPromise;
+		const movieDb = client?.db('sample_mflix');
+		const moviesCollection = movieDb?.collection('movies');
+
+		const movieArray = await moviesCollection
+			?.find({
+                $and: [
+				 { year: { $gte: 2015, $lte: 2016 }},
+                 { 'imdb.rating': { $gte: 7 }},
+                 {$or: [{ rated: 'PG' }, { rated: 'G' }]}
+            ]
+			})
+			.toArray();
+
+		movies = movieArray?.map((movie) => {
+			return {
+				id: movie._id.toString(),
+				title: movie.title,
+				year: movie.year,
+				runtime: movie.runtime,
+				plot: movie.plot,
+				rated: movie.rated,
+				imdb: {
+					rating: movie.imdb.rating
+				}
+			};
+		});
+
+		//console.log('Movies: ', movies)
+	} catch (error) {
+		console.error('MongoDB connection error:', error);
+		if (client) await client.close(); // if error, close the connection
+		return {
+			status: 500,
+			message: 'MongoDB connection error'
+		};
+	}
+	return {
+		status: 200,
+		body: movies
+	};
+};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,3 +4,11 @@
 </script>
 
 {@render children()}
+
+<div class="w-full text-center mt-6 mb-6">
+	<div class="border-4 border-gray-800 p-6 rounded-lg bg-gray-100 inline-block">
+		<h2 class="text-4xl">🦴 🦴 🦴 🦴 🦴</h2>
+		<h2 class="text-2xl text-center text-black mt-5 mb-5">💀 Enjoy my Skeleton Svelte Training App 💀</h2>
+		<h2 class="text-4xl">🦴 🦴 🦴 🦴 🦴</h2>
+	</div>
+</div>
