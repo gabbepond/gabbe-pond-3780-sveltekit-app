97c9edcb15ab24cc2a864221aa8d0a562e331b44
Gabbe Pond <114202309+gabbepond@users.noreply.github.com>
2025-02-25 17:28:06 -0700

    changes to my code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,8 @@
 	const { data, children } = $props<{ data: PageData, children: any }>();
 
 	//let { children } = $props();
+	console.log(data?.userProfile);
 </script>
+<button class="btn preset-filled rounded-md !p-6 uppercase  text-xl"><a href="/api/auth/login">Sign in</a></button>
 
 {@render children()}
--- src/routes/mongodb/+page.server.ts
+++ src/routes/mongodb/+page.server.ts
@@ -1,33 +1,29 @@
 import type { PageServerLoad } from './$types';
-
-interface Locals {
-	mongoClient: any;
-}
 import { redirect } from '@sveltejs/kit';
-
+ 
 let client
-
+ 
 export const load: PageServerLoad = async ({ parent, locals }) => {
-
-	const { isAuthenticated } = await parent();
+ 
+    const { isAuthenticated } = await parent();
          if (!isAuthenticated) {
         throw redirect(302, '/');
     }
-
-	const client = locals.mongoClient;
-	try {
-		await client?.db('sample_airbnb').command({ ping: 1 });
-		console.log('Pinged my MongoDB database.  It worked!');
-	} catch (error) {
-		console.error('MongoDB connection error:', error);
-		return {
-			status: 500,
-			message: 'MongoDB connection error'
-		};
-	}
-
-	return {
-		status: 200,
-		message: 'MongoDB connection successful'
-	};
+ 
+    const client = locals.mongoClient;
+    try {
+        await client?.db('sample_airbnb').command({ ping: 1 });
+        console.log('Pinged my MongoDB database.  It worked!');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        return {
+            status: 500,
+            message: 'MongoDB connection error'
+        };
+    }
+ 
+    return {
+        status: 200,
+        message: 'MongoDB connection successful'
+    };
 };
